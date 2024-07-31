06041c580f72bc9022e26f5d5a72ab9dcfb8d323
ViniciusDeCarli <viniciusgabriellandgraffdecarl@gmail.com>
2024-07-30 21:18:55 -0300

    Projeto atualizado

--- src/pages/estoque/index.js
+++ src/pages/estoque/index.js
@@ -51,8 +51,27 @@ export default function Estoque({navigation}) {
     return(
         <View style={styles.container}>
             <View style={styles.header}> 
+                <View style={styles.containerTitle}>
+                    <Text style={styles.titleText}>Estoque</Text>
+                </View>
 
-                <View style={{flexDirection: "row", justifyContent: "space-around", width: "100%", marginTop: 15, marginBottom: 20}}
+                <TouchableOpacity style={{...styles.buttonCadPro, width: 45, height: 45}}
+                    onPress={() =>navigation.navigate("Produto")}>
+                    <Text style={{...styles.textCadProd, fontSize: 30 }}>+</Text>
+                </TouchableOpacity>
+            </View>
+            
+            <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 10}}>
+                <View style={styles.viewInput}>
+                    <Image style={styles.imgPesq} source={require("../../images/search.png")}/>
+                    <TextInput style={{width: "90%", marginRight: 10}}
+                        placeholderTextColor={"#808080"}
+                        placeholder="Pesquisar"
+                        keyboardType="text"
+                    />
+                </View>
+
+                <View style={{flexDirection: "row", justifyContent: "space-around", width: "100%", marginTop: 15}} 
                     accessible={true} accessibilityRole="checkbox">
                     <Text>X produto cadastrados</Text>
                     <Text>Filtro ▽</Text>
