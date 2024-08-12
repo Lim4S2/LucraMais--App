80f443fa78db10ce439415c27dfaf56ce96d62c0
ViniciusDeCarli <viniciusgabriellandgraffdecarl@gmail.com>
2024-08-12 13:56:31 -0300

    Mexendo na tela de fechamento do dia

--- src/pages/fechamento/index.js
+++ src/pages/fechamento/index.js
@@ -6,28 +6,29 @@ export default function Fechamento({ navigation }) {
     return (
         <View style={{alignItems: "center"}}>
             <View style={styles.container}>
+                <Image source={require("../../images/iconFechaCaixa.png")}
+                style={{ width: 40, height: 40, marginRight: 20 }}/>
                 <Text style={styles.text}>Fechamento do dia</Text>
             </View>
 
-            <View style={styles.viewHora}>
-                <Text>Caixa aberto - 
+                <Text style={{fontSize: 18, marginTop: 20}}>Caixa aberto - 
                     <Text>_</Text>
-                    <Text>08:33</Text>
+                    <Text style={{fontSize: 18,marginTop: 10}}>08:33</Text>
                 </Text>
-                <Text>Fechou caixa - 
+                <Text style={{fontSize: 18,marginTop: 10}}>Fechou caixa - 
                     <Text>_</Text>
-                    <Text>11:40</Text>
+                    <Text style={{fontSize: 18,marginTop: 10}}>11:40</Text>
                 </Text>
-                <Text>Horas trabalhadas - 
+                <Text style={{fontSize: 18,marginTop: 10}}>Horas trabalhadas - 
                     <Text>_</Text>
-                    <Text>03:07</Text>
+                    <Text style={{fontSize: 18,marginTop: 10}}>03:07</Text>
                 </Text>
-            </View>
 
             <View style={styles.viewLucro}>
-                <Text style={{...styles.text, fontSize: 22}}>Lucro do dia</Text>
-                <Text>R$
-                    <Text>3.570,00</Text>
+                <Text style={{...styles.text, fontSize: 20}}>Lucro do dia</Text>
+                <Text style={{...styles.text, fontSize: 28, color: "green"}}>R$
+                    <Text style={{color: "white"}}>_</Text>
+                    <Text style={{...styles.text, fontSize: 40, color: "green"}}>3.570,00</Text>
                 </Text>
             </View>
 
--- src/pages/fechamento/style.js
+++ src/pages/fechamento/style.js
@@ -4,11 +4,23 @@ const styles = StyleSheet.create({
     container: {
         width: "100%",
         marginTop: "10%",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: 'center',
         borderBottomWidth: 2,
         borderColor: "#545454",
         padding: 10
+    },
+    viewLucro: {
+        width: "90%",
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 50,
+        backgroundColor: "white",
+        elevation: 15
     },
     text: {
         fontSize: 26,
