cf5832c4aea987dc8132e420f0eab08a033d4f12
ViniciusDeCarli <viniciusgabriellandgraffdecarl@gmail.com>
2024-10-01 20:16:56 -0300

    Colocando gráfico na minha Branch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
-import React, {useEffect} from "react";
-import {View, Text, BackHandler} from "react-native";
+import React, {useEffect} from "react"
+import {View, Text, BackHandler} from "react-native"
+import { BarChart, PieChart } from "react-native-gifted-charts"
 import styles from "./style"
 
 export default function Home({navigation}) {
@@ -12,10 +13,38 @@ export default function Home({navigation}) {
     }, [])
 
     return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Tela inicial</Text>
-            <View>
-                
+        <View >
+            <View style={styles.container}>
+                <Text style={styles.text}>Tela inicial</Text>
+            </View>
+
+            <View style={{padding: 15}}>
+                <BarChart
+                    data={[
+                        {value: 500, frontColor: 'blue'},
+                        {value: 800, frontColor: 'green'},
+                        {value: 300, frontColor: 'red'},
+                        {value: 0, frontColor: 'purple'},
+                    ]}
+                    height={250}
+                    width={320}
+                    // grossura do gráfico
+                    barWidth={40}
+                    // minimo de altura quando for 0
+                    minHeight={3}
+                    barBorderRadius={3}
+                    spacing={20}
+                    // quantidade de números do lado
+                    noOfSections={3}
+                    // linha preta que fica no lado
+                    yAxisThickness={0}
+                    // linha preta que fica embaixo
+                    xAxisThickness={0}
+                    yAxisTextStyle={{color: "gray"}}
+                    xAxisLabelTextStyle={{color: "gray"}}
+                    isAnimated
+                    dashGap={5}
+                />
             </View>
         </View>
     )
