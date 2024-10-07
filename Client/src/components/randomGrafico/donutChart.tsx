0bb9e8182f33db4a7108826e32669e3f56b0fb80
ViniciusDeCarli <viniciusgabriellandgraffdecarl@gmail.com>
2024-10-07 15:27:52 -0300

    Continuação da continuação da continuação

--- Client/src/components/randomGrafico/donutChart.tsx
+++ Client/src/components/randomGrafico/donutChart.tsx
@@ -1,10 +1,34 @@
 import { View, Text, StyleSheet } from "react-native"
 import React from "react"
+import { SharedValue } from "react-native-reanimated";
+import { Canvas, Path, Skia } from "@shopify/react-native-skia"
 
-const DonutChart = (props) => {
+type Props = {
+    radius: number;
+    strokeWidth: number;
+    outerStrokeWidth: number;
+    totalValue: SharedValue<number>;
+}
+
+const DonutChart = ({radius, strokeWidth, outerStrokeWidth, totalValue} : Props) => {
+    
+    const innerRadius = radius - outerStrokeWidth / 2
+    const path = Skia.Path.Make()
+    path.addCircle(radius, radius, innerRadius)
+    
     return(
         <View>
-            <Text>Donut Chart</Text>
+            <Canvas>
+                <Path path={path} 
+                    color="#acc4cc"
+                    style="stroke"
+                    strokeWidth={outerStrokeWidth}
+                    strokeJoin="round"
+                    strokeCap="round"
+                    start={0}
+                    end={1}
+                />
+            </Canvas>
         </View>
     )
 }
