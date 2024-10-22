a5ad1e51aea74c43ea60f0f38baad98172e8cfbd
ViniciusDeCarli <viniciusgabriellandgraffdecarl@gmail.com>
2024-10-22 09:40:04 -0300

    Mexendo na tela Conta

--- Client/src/pages/conta/index.js
+++ Client/src/pages/conta/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Image, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker'
+import { Feather } from "@expo/vector-icons"
 import styles from './style';
 
 export default function Conta({navigation}) {
@@ -45,13 +46,30 @@ export default function Conta({navigation}) {
 
             <View style={styles.optionContainer}>
                 <View style={{ width: '100%' }}>
-                    <View>
+                    <View style={{flexDirection: 'row', width: '100%', height: 'auto', gap: 20, justifyContent: "flex-start"}}>
+                        <Feather name='user' size={20}/>
                         <Text>Conta e informações pessoais</Text>
                     </View>
-                    <Text>Configurações</Text>
-                    <Text>Sobre o aplicativo</Text>
-                    <Text>Suporte</Text>
-                    <Text>Sair</Text>
+                        
+                    <View style={{flexDirection: 'row', width: '100%', height: 'auto', gap: 20, justifyContent: "flex-start"}}>
+                        <Feather name='settings' size={20}/>
+                        <Text>Configurações</Text>
+                    </View>
+                        
+                    <View style={{flexDirection: 'row', width: '100%', height: 'auto', gap: 20, justifyContent: "flex-start"}}>
+                        <Feather name='' size={20}/>
+                        <Text>Sobre o aplicativo</Text>
+                    </View>
+                    
+                    <View style={{flexDirection: 'row', width: '100%', height: 'auto', gap: 20, justifyContent: "flex-start"}}>
+                        <Feather name='' size={20}/>
+                        <Text>Suporte</Text>
+                    </View>
+                    
+                    <View style={{flexDirection: 'row', width: '100%', height: 'auto', gap: 20, justifyContent: "flex-start"}}>
+                        <Feather name='log-out' size={20}/>
+                        <Text>Sair</Text>
+                    </View>
                 </View>
             </View>
         </View>
--- Client/src/pages/preload/index.js
+++ Client/src/pages/preload/index.js
@@ -1,34 +1,24 @@
-import React, { useEffect } from "react";
-import { View, Image, ActivityIndicator, StatusBar, Text } from "react-native";
+import React from "react";
+import { View, Image, StatusBar, Text } from "react-native";
 import LottieView from "lottie-react-native"
 import styles from "./style";
 
 export default function Preload({ navigation }) {
-    /*useEffect(() => {
-        // Temporizador para navegação após 2 segundos
-        const timer = setTimeout(() => {
-            navigation.navigate("Login"); // Navega para a tela de Login após 2 segundos
-        }, 20000);
-
-        return () => clearTimeout(timer); // Limpa o timer quando o componente desmonta
-    }, [navigation]);*/
 
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor={"#04414b"} barStyle="light-content"/>
             <Image source={require("../../images/logo.png")} style={styles.logo} />
 
-            {/*<View style={styles.carregamento}>
-                <ActivityIndicator size="large" color="#0000ff" />
-            </View>*/}
-
             <LottieView
-                source={require("../../images/animacaoCaminhao.json")}
+                source={require("../../images/animacaoOficial.json")}
                 autoPlay={true}
                 onAnimationFinish={() => navigation.navigate("Login")}
                 style={{width: 120, height: 120}}
                 loop={false}
             />
+
+            <Text onPress={() => navigation.navigate("Login")}>Próxima página</Text>
         </View>
     );
 }
